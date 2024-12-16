c5553fd83ad4a9fcf774f5ecdc97385150cd980b
Siddharth Srivastava <143588718+Siddharth4501@users.noreply.github.com>
2024-12-16 13:40:00 +0530

    Added Sabstance creation function

--- Server/controllers/Substance.controller.js
+++ Server/controllers/Substance.controller.js
@@ -19,6 +19,34 @@ const SubstanceData=async(req,res,next)=>{
     
 }
 
+const SubstanceAdd=async(req,res,next)=>{
+    try{
+        const {Substance_Name,Method_Of_Analysis,Unit_Of_Measurement}=req.body
+        if(!Substance_Name || !Method_Of_Analysis || !Unit_Of_Measurement){
+            return next(new AppError('All fiels are required',400))
+        }
+        const substance=await Substance.create({
+            Substance_Name,
+            Method_Of_Analysis,
+            Unit_Of_Measurement
+        })
+
+        if(!substance){
+            return(new AppError('Substance could not be created ,please try again',400))
+        }
+        await substance.save()
+        res.status(200).json({
+            success:true,
+            message:'Substance Data Added Successfully',   
+            substance,
+        })
+    }
+    catch(e){
+        return next(new AppError(e.message,500))
+    }
+    
+}
+
 const SubstanceEdit=async(req,res,next)=>{
     res.status(200).json({
         success:true,
@@ -29,4 +57,5 @@ const SubstanceEdit=async(req,res,next)=>{
 export {
     SubstanceData,
     SubstanceEdit,
+    SubstanceAdd,
 }
