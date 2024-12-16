72b8bb0a6987c9f66f5eb672a4d9e1b9e62602e8
Siddharth Srivastava <143588718+Siddharth4501@users.noreply.github.com>
2024-12-16 16:50:52 +0530

    Added save and fetch group data funtion respetively

--- Server/controllers/Group.controller.js
+++ Server/controllers/Group.controller.js
@@ -1,13 +1,29 @@
+import Group from "../models/Group.model.js";
 import AppError from "../utils/error.utils.js";
 
 const GroupAdd=async(req,res,next)=>{
+    const {Group_Name,Type_Of_Testing,Tests}=req.body;
+    if(!Group_Name || !Type_Of_Testing || !Tests){
+        return next(new AppError('All fields are required',400));
+    }
+    const group=await Group.create({
+        Group_Name,
+        Type_Of_Testing,
+        Tests
+    })
+    if(!group){
+        return next(new AppError('Group cannot be created'))
+    }
+    await group.save()
     res.status(200).json({
         success:true,
         message:'Group Added Successfully',
+        group,
     })
 }
 
 const GroupUpdate=async(req,res,next)=>{
+    
     res.status(200).json({
         success:true,
         message:'Group Updated Successfully',
@@ -15,9 +31,14 @@ const GroupUpdate=async(req,res,next)=>{
 }
 
 const GroupData=async(req,res,next)=>{
+    const group=await Group.find({})
+    if(!group){
+        return next(new AppError('Group cannot be created'))
+    }
     res.status(200).json({
         success:true,
         message:'Group Data fetched Successfully',
+        group,
     })
 }
 
