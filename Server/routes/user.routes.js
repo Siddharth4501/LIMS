358318cdb327ac0cc38d71e12b97d9fc7f30e28a
Siddharth Srivastava <143588718+Siddharth4501@users.noreply.github.com>
2024-12-17 12:44:02 +0530

    Added /daya route

--- Server/routes/user.routes.js
+++ Server/routes/user.routes.js
@@ -1,9 +1,10 @@
 import {Router} from "express";
-import { Login,Register,Logout } from "../controllers/user.controller.js";
+import { Login,Register,Logout, UserData } from "../controllers/user.controller.js";
 
 const router=Router()
 
 router.post('/register',Register)
 router.post('/login',Login)
 router.get('/logout',Logout)
+router.get('/data',UserData)
 export default router
