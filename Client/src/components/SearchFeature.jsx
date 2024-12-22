0aadb300903ab2069b385fe45d19dc8662fe6620
Prince Kushwaha <143611059+imprince-kushwaha@users.noreply.github.com>
2024-12-22 20:12:33 +0530

    Added Search Feature
    Search feature in Register sample

--- Client/src/components/SearchFeature.jsx
+++ Client/src/components/SearchFeature.jsx
@@ -1,18 +1,21 @@
 import React, { useState, useEffect } from 'react';
 
-function SearchFeature() {
+function SearchFeature({data}) {
     const [query, setQuery] = useState('');
     const [filteredItems, setFilteredItems] = useState([]);
 
-    const items = ['apple', 'banana', 'orange',
-        'grape', 'watermelon'];
-
+    console.log("data",data)
     useEffect(() => {
-        const filtered = items.filter(item =>
+        const filtered = data.filter(item =>
             item.toLowerCase().includes(query.toLowerCase())
         );
         setFilteredItems(filtered);
     }, [query]);
+    // const filtered = Object.keys(data).filter((key) =>
+    //     key.toLowerCase().includes(query.toLowerCase())
+    //   );
+    //   onSearch(filtered);
+    // }, [query, data, onSearch]);
 
     return (
         <div className='p-4'>
@@ -20,12 +23,10 @@ function SearchFeature() {
                 onChange={(e) => setQuery(e.target.value)}
                 className="p-1 border border-gray-300 rounded mb-2"
             />
-            <ul>
                 {
-                    filteredItems.length > 0 ? filteredItems.map((item, index) => (<li key={index}>{item}</li>)) :
+                    filteredItems.length > 0 ? filteredItems.map((item, index) => (<div key={index}>{item}</div>)) :
                                     (<p className="text-gray-500">No results found</p>)
                 }
-            </ul>
         </div>
     );
 }
