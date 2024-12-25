2707ada6a967ce51dbf192cf53a75d2b5f177085
Siddharth Srivastava <143588718+Siddharth4501@users.noreply.github.com>
2024-12-25 18:27:51 +0530

    changed ri=oute name of sample/register

--- Server/routes/Sample.routes.js
+++ Server/routes/Sample.routes.js
@@ -2,7 +2,7 @@ import {Router} from "express";
 import { SampleRegister,SampleData,SampleEdit } from "../controllers/Sample.controller.js";
 const router=Router()
 
-router.post('/registration',SampleRegister)
+router.post('/register',SampleRegister)
 router.get('/data',SampleData)
 router.get('/edit',SampleEdit)
 export default router;
