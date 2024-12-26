e3a562ab1f6c5cdec02c8628c9f14a7d1bbcb305
Siddharth Srivastava <143588718+Siddharth4501@users.noreply.github.com>
2024-12-26 12:21:18 +0530

    fixed indexing of samples

--- Client/src/components/Samples.jsx
+++ Client/src/components/Samples.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 
-const Samples = ({data}) => {
+const Samples = ({data,index}) => {
     const navigate=useNavigate();
     const handleViewMore=()=>{
         navigate('/SampleAllotment/View_More',{state:{...data}})
@@ -11,13 +11,13 @@ const Samples = ({data}) => {
           
           <tbody>
               <tr className="hover:bg-gray-100">
+              <td className="border border-gray-300 px-4 py-2 text-center">{index+1}</td>
               <td className="border border-gray-300 px-4 py-2 text-center">{data.Registration_Number}</td>
-              <td className="border border-gray-300 px-4 py-2 text-center">12345</td>
               <td className="border border-gray-300 px-4 py-2 text-center">{data.Name}</td>
               <td className="border border-gray-300 px-4 py-2 text-center">{data.Storage_Conditions}</td>
               <td className="border border-gray-300 px-4 py-2 text-center">{data.Date.split('T')[0]}</td>
               <td className="border border-gray-300 px-4 py-2 text-center">                 
-                <button className="text-indigo-500 hover:text-indigo-700 underline" onClick={handleViewMore}>View </button>
+                <button className="bg-indigo-500 hover:bg-indigo-700 rounded-md text-white pl-4 pr-4 pt-1 pb-1" onClick={handleViewMore}>View </button>
               </td>
             </tr>
           </tbody>
