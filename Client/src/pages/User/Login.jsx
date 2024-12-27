885b87490dbaf441a2bcb65e24bb4468df37d940
Siddharth Srivastava <143588718+Siddharth4501@users.noreply.github.com>
2024-12-27 21:28:42 +0530

    Worked on Login Action

--- Client/src/pages/User/Login.jsx
+++ Client/src/pages/User/Login.jsx
@@ -1,17 +1,49 @@
-import React, { Children, useState } from 'react'
+import React, { useState } from 'react'
+import { toast } from "react-hot-toast";
+import { useDispatch } from "react-redux";
+import {  useNavigate } from "react-router-dom";
+import { login } from "../../Redux/Slices/AuthSlice";
 import { MdEmail } from "react-icons/md";
 import { RiLockPasswordFill } from "react-icons/ri";
 import { FaEyeSlash } from "react-icons/fa";
 import { FaEye } from "react-icons/fa";
 const Login = () => {
+    const dispatch=useDispatch();
+    const navigate=useNavigate();
     const [showPassword,setShowPassword]=useState(false);
     const handlePasswordToggle=()=>{
         setShowPassword((prevState)=>!prevState);
     }
+
+    // function to login
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        const email=e.target[0].value
+        const password=e.target[1].value
+        console.log(email,password)
+
+        // checking the empty fields
+        if (!email || !password) {
+        toast.error("Please fill all the fields");
+        return;
+        }
+        const loginData={
+            "email":email,
+            "password":password
+        }
+        // calling login action
+        const res = await dispatch(login(loginData));
+
+        // redirect to home page if true
+        if (res?.payload?.success) navigate("/");
+
+
+    };
   return (
     <div className='flex flex-col font-bold h-screen w-screen justify-center'>
         <center><h1 className='text-4xl italic bg-gray-300 p-3'>Login Page</h1></center>
-        <form className='grid gap-2 border border-gray-700 rounded-lg h-80 w-1/3 m-auto p-4 bg-gray-300'>
+        <form onSubmit={handleLogin} className='grid gap-2 border border-gray-700 rounded-lg h-80 w-1/3 m-auto p-4 bg-gray-300'>
             <div className='w-full p-2 pt-5 flex'>
                 <span className='pt-2 p-1'><MdEmail /></span>
                 <label htmlFor="Email" className='w-1/4 text-xl'>Email:</label>
