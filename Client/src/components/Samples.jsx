db021452a5d4d952d6ab49c4d27328fc03c08881
Siddharth Srivastava <143588718+Siddharth4501@users.noreply.github.com>
2024-12-28 23:54:27 +0530

    added condition based navigation

--- Client/src/components/Samples.jsx
+++ Client/src/components/Samples.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 
-const Samples = ({data,index}) => {
+const Samples = ({difference,data,index}) => {
     const navigate=useNavigate();
-    const handleViewMore=()=>{
-        navigate('/SampleAllotment/View_More',{state:{...data}})
+    const handleRedirection=()=>{
+        if(difference==='All Sample History'){
+          navigate('/AllSampleHistory/View_More',{state:{...data}})
+        }
+        else{
+          navigate('/SampleAllotment/View_More',{state:{...data}})
+        }
+        
       }
   return (
           
@@ -14,10 +20,10 @@ const Samples = ({data,index}) => {
               <td className="border border-gray-300 px-4 py-2 text-center">{index+1}</td>
               <td className="border border-gray-300 px-4 py-2 text-center">{data.Registration_Number}</td>
               <td className="border border-gray-300 px-4 py-2 text-center">{data.Name}</td>
-              <td className="border border-gray-300 px-4 py-2 text-center">{data.Storage_Conditions}</td>
+              <td className="border border-gray-300 px-4 py-2 text-center">{data.Group}</td>
               <td className="border border-gray-300 px-4 py-2 text-center">{data.Date.split('T')[0]}</td>
               <td className="border border-gray-300 px-4 py-2 text-center">                 
-                <button className="bg-indigo-500 hover:bg-indigo-700 rounded-md text-white pl-4 pr-4 pt-1 pb-1" onClick={handleViewMore}>View </button>
+                <button className="bg-indigo-500 hover:bg-indigo-700 rounded-md text-white pl-4 pr-4 pt-1 pb-1" onClick={handleRedirection}>View </button>
               </td>
             </tr>
           </tbody>
