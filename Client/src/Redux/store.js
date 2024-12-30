dd0efb81e6a033484bbcb8b5c650c0230a4a64dc
Siddharth Srivastava <143588718+Siddharth4501@users.noreply.github.com>
2024-12-31 01:43:58 +0530

    Added sample reducer in store

--- Client/src/Redux/store.js
+++ Client/src/Redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSliceReducer from './Slices/AuthSlice.js'
 import groupSliceReducer from './Slices/GroupSilce.js'
+import sampleSliceReducer from './Slices/SampleSlice.js'
 
 const store=configureStore({
     reducer:{
         auth:authSliceReducer,
         group:groupSliceReducer,
+        sample:sampleSliceReducer
     },
     devtools:true
 });
