33eedea12ce81f0089374e0a7d415c9c097c568f
Siddharth Srivastava <143588718+Siddharth4501@users.noreply.github.com>
2025-01-02 14:52:29 +0530

    Added route to save TM data

--- Server/routes/Sample.routes.js
+++ Server/routes/Sample.routes.js
@@ -1,8 +1,9 @@
 import {Router} from "express";
-import { SampleRegister,SampleData,SampleEdit } from "../controllers/Sample.controller.js";
+import { SampleRegister,SampleData,SampleEdit,TMDataSave } from "../controllers/Sample.controller.js";
 const router=Router()
 
 router.post('/register',SampleRegister)
+router.post('/TM/data/save',TMDataSave)
 router.get('/data',SampleData)
 router.get('/edit',SampleEdit)
 export default router;
