da719adae28f58ee1fd2eb4b78bd846606961534
Siddharth Srivastava <143588718+Siddharth4501@users.noreply.github.com>
2025-01-03 18:48:23 +0530

    Added Routes for getting TechnicalManager or Analyst data

--- Server/routes/Sample.routes.js
+++ Server/routes/Sample.routes.js
@@ -1,9 +1,10 @@
 import {Router} from "express";
-import { SampleRegister,SampleData,SampleEdit,TMDataSave } from "../controllers/Sample.controller.js";
+import { SampleRegister,SampleData,SampleEdit,TMDataSave, TMANData } from "../controllers/Sample.controller.js";
 const router=Router()
 
 router.post('/register',SampleRegister)
 router.post('/TM/data/save',TMDataSave)
+router.get('/TMAN/data',TMANData)
 router.get('/data',SampleData)
 router.get('/edit',SampleEdit)
 export default router;
