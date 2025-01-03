61fc2a84a3824b4d3125941238b0967182ce42f7
Siddharth Srivastava <143588718+Siddharth4501@users.noreply.github.com>
2025-01-03 18:48:51 +0530

    mades changes in Technical Manager Schema

--- Server/models/TechManager_Analyst.model.js
+++ Server/models/TechManager_Analyst.model.js
@@ -1,19 +1,29 @@
-import {model,Schema} from 'mongoose'
+import {model,Schema,mongoose} from 'mongoose'
 
 const techManager_AnalystSchema=new Schema(
     {
-        Substances_To_Be_Analysed:[
-            {
-                Substance_Name:String,
-                Method:String,
-                Unit_Of_Measurement:String,
-                Name_Of_Analayst:String,
-                Result:{
-                    type:Number,
-                    default:0,
+        Sample_Alloted:{ type: mongoose.Schema.Types.ObjectId, ref: "Sample" },
+        Substances_To_Be_Analysed: {
+            type: "object",
+            patternProperties: {// used to dynamically add key
+              "^[A-Za-z]+$": {
+                type: "array",
+                items: {
+                  type: "object",
+                  properties: {
+                    Test: { type: "string" },
+                    Method: { type: "string" },
+                    Unit: { type: "string" },
+                    Analyst: { type: "string" },
+                    Result: { type: "number", default: 0 }
+                  },
                 }
-        },],
-
+              }
+            },
+          },
+        
+        TM_Status:String,
+        An_Status:String,
         Due_Date:{
             type:Date,
             required:[true,'Due Date is required'],
