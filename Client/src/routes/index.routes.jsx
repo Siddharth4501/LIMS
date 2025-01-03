aa48217811f01c25f9cfe4e135649163367d3245
Siddharth Srivastava <143588718+Siddharth4501@users.noreply.github.com>
2025-01-03 18:49:43 +0530

    Added some routes

--- Client/src/routes/index.routes.jsx
+++ Client/src/routes/index.routes.jsx
@@ -8,6 +8,10 @@ import UserInterface from "../pages/User/UserInterface"
 import AllSamplesHistory from "../pages/SampleRegistration/AllSamplesHistory"
 import FullHistory from "../pages/SampleRegistration/FullHistory"
 import UserSampleRegister from "../pages/User/UserSampleRegister"
+import AN_PendingSamplesViewMore from "../pages/Analyst/AN_PendingSamplesViewMore"
+import AN_PendingSamples from "../pages/Analyst/AN_PendingSamples"
+import AN_CompletedSamples from "../pages/Analyst/AN_CompletedSamples"
+import AnalystHome from "../pages/Analyst/AnalystHome"
 
 const router = createBrowserRouter([
     {
@@ -46,6 +50,27 @@ const router = createBrowserRouter([
         path: "/AllSampleHistory/View_More",
         element:<FullHistory/> ,
     },
+    {
+        path:"/AN_PendingSample/ViewMore",
+        element:<AN_PendingSamplesViewMore/>
+    },
+    {
+        path:"/AN_PendingSamples",
+        element:<AN_PendingSamples/>
+    },
+    {
+        path:"/AN_CompletedSamples",
+        element:<AN_CompletedSamples/>
+    },
+    // {
+    //     path:"/AN_CompletedSamples/ViewMore",
+    //     element:<AN_CompletedSamples/>
+    // },
+    {
+        path:"/Analyst/Home",
+        element:<AnalystHome/>
+    },
+
 ])
 
 export default router
