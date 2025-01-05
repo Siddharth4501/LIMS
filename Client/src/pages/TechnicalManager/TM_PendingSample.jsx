6675e555b0bbf9173bf7ae074b2d799275083ccb
Siddharth Srivastava <143588718+Siddharth4501@users.noreply.github.com>
2025-01-05 14:09:21 +0530

    worked on Tech Manager Pending Samples At Analyst Page

--- Client/src/pages/TechnicalManager/TM_PendingSample.jsx
+++ Client/src/pages/TechnicalManager/TM_PendingSample.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React,{ useEffect }from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { getTMANData,getSampleData } from '../../Redux/Slices/SampleSlice'
+import { useNavigate } from 'react-router-dom';
 
 const TM_PendingSample = () => {
+  const { TmAnData,sampleData }=useSelector((state)=>state.sample)
+  const dispatch=useDispatch();
+  const navigate=useNavigate();
+  useEffect(() => {
+      (async () => {
+      await dispatch(getTMANData());
+      await dispatch(getSampleData());
+      })();
+  }, []);
+  console.log(TmAnData,sampleData);
   return (
     <div>
-      <h1>TM_PendingSample</h1>
+      <div className='w-screen text-center pt-2 text-3xl font-bold'>Pending Samples At Analyst</div>
+      <br /><br />
+      <div>
+        <table className='table-auto w-full border-collapse border border-gray-300'>
+          <thead>
+            <tr className="bg-slate-200">
+                <th className="border border-gray-300 px-4 py-2 text-center">S.No.</th>
+                <th className="border border-gray-300 px-4 py-2 text-center">Registration No.</th>
+                <th className="border border-gray-300 px-4 py-2 text-center">Sample Name</th>
+                <th className="border border-gray-300 px-4 py-2 text-center">Due Date</th>
+                <th className="border border-gray-300 px-4 py-2 text-center">Storage Condition</th>
+                <th className="border border-gray-300 px-4 py-2 text-center">Registration Date</th>
+                <th className="border border-gray-300 px-4 py-2 text-center">Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              TmAnData?.filter((data)=>data.AN_Status === 'Pending At Analyst').map((item,index)=>{
+                let fliteredSample=sampleData?.filter((data)=>data._id== item.Sample_Alloted)
+                if(!fliteredSample){
+                  return null;
+                }
+                {console.log(fliteredSample,"kiuku")}
+                return(
+                  <tr className="hover:bg-gray-100" key={item._id}>
+                    <td className="border border-gray-300 px-4 py-2 text-center">{index+1}</td>
+                    <td className="border border-gray-300 px-4 py-2 text-center">{fliteredSample[0]?.Registration_Number}</td>
+                    <td className="border border-gray-300 px-4 py-2 text-center">{fliteredSample[0]?.Name}</td>
+                    <td className="border border-gray-300 px-4 py-2 text-center">{item.Due_Date.split('T')[0]}</td>
+                    <td className="border border-gray-300 px-4 py-2 text-center">{fliteredSample[0]?.Storage_Conditions}</td>
+                    <td className="border border-gray-300 px-4 py-2 text-center">{fliteredSample[0]?.Date.split('T')[0]}</td>
+                    <td className="border border-gray-300 px-4 py-2 text-center">{item.TM_Status}</td>
+                    
+                  </tr>
+                )
+                })
+              }
+              
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }
