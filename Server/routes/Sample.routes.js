b489f1a42159e6fa4187de950dfcbc5d87e9c29a
Siddharth Srivastava <143588718+Siddharth4501@users.noreply.github.com>
2025-01-05 14:03:36 +0530

    Added route to update Tech Manager/Analyst Data

--- Server/routes/Sample.routes.js
+++ Server/routes/Sample.routes.js
@@ -1,5 +1,5 @@
 import {Router} from "express";
-import { SampleRegister,SampleData,SampleEdit,TMDataSave, TMANData } from "../controllers/Sample.controller.js";
+import { SampleRegister,SampleData,SampleEdit,TMDataSave, TMANData,TMANDataUpdate } from "../controllers/Sample.controller.js";
 const router=Router()
 
 router.post('/register',SampleRegister)
@@ -7,4 +7,5 @@ router.post('/TM/data/save',TMDataSave)
 router.get('/TMAN/data',TMANData)
 router.get('/data',SampleData)
 router.get('/edit',SampleEdit)
+router.put('/TMAN/data/update',TMANDataUpdate)
 export default router;
