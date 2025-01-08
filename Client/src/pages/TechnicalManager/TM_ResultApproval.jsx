08a38ab73e3938dee6947c9e60c9236f7260a961
Siddharth Srivastava <143588718+Siddharth4501@users.noreply.github.com>
2025-01-08 21:36:08 +0530

    fiexed some bugs

--- Client/src/pages/TechnicalManager/TM_ResultApproval.jsx
+++ Client/src/pages/TechnicalManager/TM_ResultApproval.jsx
@@ -14,12 +14,15 @@ const TM_ResultApproval = () => {
       })();
   }, []);
   console.log(TmAnData,sampleData)
-  const handleNavigation=(data,fliteredSample,ID)=>{
-    navigate("/ResultApproval/View_More",{state:{...data,...fliteredSample,ID}})
+  const handleNavigation=(TMANdata,fliteredSample,TMANID)=>{
+    navigate("/ResultApproval/Result_Status",{state:{...TMANdata,...fliteredSample,TMANID}})
   }
   return (
     <div>
-      <div className='w-screen text-center pt-2 text-3xl font-bold'>Pending Approval Page</div>
+      <div className='w-full flex border bg-gray-300 p-5'>
+            <div className='w-3/5 text-3xl font-bold'><span className='float-right'>Pending Approval Page</span></div>
+            <div className='w-2/5'><button className='bg-indigo-700 px-4 py-1 text-white rounded-md float-right' onClick={()=>navigate('/')}>Back</button></div>
+        </div>
       <br /><br />
       <div>
         <table className='table-auto w-full border-collapse border border-gray-300'>
@@ -37,7 +40,7 @@ const TM_ResultApproval = () => {
           </thead>
           <tbody>
             {
-              TmAnData?.filter((data)=>data.AN_Status === 'Pending For Approval At Technical Manager').map((item,index)=>{
+              TmAnData?.filter((data)=>data.TM_Status === 'Pending For Approval At TM').map((item,index)=>{
                 let fliteredSample=sampleData?.filter((data)=>data._id== item.Sample_Alloted)
                 if(!fliteredSample){
                   return null;
@@ -51,7 +54,7 @@ const TM_ResultApproval = () => {
                     <td className="border border-gray-300 px-4 py-2 text-center">{item.Due_Date.split('T')[0]}</td>
                     <td className="border border-gray-300 px-4 py-2 text-center">{fliteredSample[0]?.Storage_Conditions}</td>
                     <td className="border border-gray-300 px-4 py-2 text-center">{fliteredSample[0]?.Date.split('T')[0]}</td>
-                    <td className="border border-gray-300 px-4 py-2 text-center">{item.AN_Status}</td>
+                    <td className="border border-gray-300 px-4 py-2 text-center">{item.TM_Status}</td>
                     <td className="border border-gray-300 px-4 py-2 text-center"><button type="button" className='bg-indigo-700 text-white px-4 py-1 rounded-md hover:bg-indigo-900' onClick={()=>handleNavigation(item,fliteredSample[0],item._id)}>View</button></td>
                   </tr>
                 )
