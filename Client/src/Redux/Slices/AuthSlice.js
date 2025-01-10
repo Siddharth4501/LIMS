3607167c716fa054a6f1d9053ad7e8b7dbf63bf1
Siddharth Srivastava <143588718+Siddharth4501@users.noreply.github.com>
2025-01-10 18:44:49 +0530

    added function to change password

--- Client/src/Redux/Slices/AuthSlice.js
+++ Client/src/Redux/Slices/AuthSlice.js
@@ -32,7 +32,7 @@ export const login = createAsyncThunk("auth/login", async (data) => {
   export const logout = createAsyncThunk("auth/logout", async () => {
     try {
       
-      let res=axios.post("http://localhost:5001/api/v1/user/logout",data) 
+      let res=axios.get("http://localhost:5001/api/v1/user/logout")
       await toast.promise(res, {
         loading: "Loading...",
         success: (data) => {
@@ -52,7 +52,7 @@ export const login = createAsyncThunk("auth/login", async (data) => {
 
   export const getAllUserData = createAsyncThunk("User/data", async () => {
     try {
-      let res=axios.get("http://localhost:5001/api/v1/User/data")//here await is not used purposely because of the following toast syntax
+      let res=axios.get("http://localhost:5001/api/v1/user/data")//here await is not used purposely because of the following toast syntax
   
       // getting response resolved here
       res = await res;//when promise is resolved it will give data
@@ -61,6 +61,30 @@ export const login = createAsyncThunk("auth/login", async (data) => {
       toast.error(error.response.data.message);
     }
   });
+
+  // function to change user password
+export const changePassword = createAsyncThunk(
+  "/auth/changePassword",
+  async (userPassword) => {
+    try {
+      let res = axios.post("http://localhost:5001/api/v1/user/Change-Password", userPassword);
+
+      await toast.promise(res, {
+        loading: "Loading...",
+        success: (data) => {
+          return data?.data?.message;
+        },
+        error: "Failed to change password",
+      });
+
+      // getting response resolved here
+      res = await res;
+      return res.data;
+    } catch (error) {
+      toast.error(error?.response?.data?.message);
+    }
+  }
+);
 
   const authSlice=createSlice({
     name:'auth',
@@ -73,13 +97,13 @@ export const login = createAsyncThunk("auth/login", async (data) => {
           localStorage.setItem("userData", JSON.stringify(action?.payload?.user));
           localStorage.setItem("isLoggedIn", true);
           state.isLoggedIn = true;
-          state.data = action?.payload?.user;
+          state.userData = action?.payload?.user;
         })
         // for user logout
         .addCase(logout.fulfilled, (state) => {
           localStorage.clear();
           state.isLoggedIn = false;
-          state.data = {};
+          state.userData = {};
         })
         .addCase(getAllUserData.fulfilled,(state,action)=>{
           if (action.payload) {
