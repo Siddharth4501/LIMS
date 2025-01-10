221463aadad7f8dee3814d346680a4bb407221a3
Siddharth Srivastava <143588718+Siddharth4501@users.noreply.github.com>
2025-01-10 18:37:56 +0530

    added rounte to handle Change Password api

--- Server/routes/user.routes.js
+++ Server/routes/user.routes.js
@@ -1,10 +1,12 @@
 import {Router} from "express";
-import { Login,Register,Logout, UserData } from "../controllers/user.controller.js";
+import { Login,Register,Logout, UserData, changePassword } from "../controllers/user.controller.js";
+import { isLoggedIn } from "../middlewares/auth.middleware.js";
 
 const router=Router()
 
 router.post('/register',Register)
 router.post('/login',Login)
+router.post('/Change-Password',isLoggedIn,changePassword)
 router.get('/data',UserData)
 router.get('/logout',Logout)
 export default router
