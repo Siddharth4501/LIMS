c993b67f38a8d17121fcd63a06f88ac6c4a99867
Siddharth Srivastava <143588718+Siddharth4501@users.noreply.github.com>
2025-01-11 00:59:22 +0530

    changed css

--- Client/src/pages/User/Login.jsx
+++ Client/src/pages/User/Login.jsx
@@ -42,8 +42,8 @@ const Login = () => {
     };
   return (
     <div className="flex flex-col font-bold h-screen w-screen justify-center">
-        <center><h1 className='text-4xl bg-gray-300 p-5'>Login Page</h1></center>
-        <form onSubmit={handleLogin} className='grid gap-2 border border-gray-700 rounded-lg h-80 w-1/3 m-auto p-4 bg-slate-400'>
+        <form onSubmit={handleLogin} className='grid gap-2 border border-gray-700 rounded-lg h-96 w-2/5 m-auto p-4 bg-slate-400'>
+        <center><h1 className='text-4xl'>Login Page</h1></center>
             <div className='w-full p-2 pt-5 flex'>
                 <span className='pt-2 p-1'><MdEmail /></span>
                 <label htmlFor="Email" className='w-1/4 text-xl'>Email:</label>
@@ -56,7 +56,7 @@ const Login = () => {
                 <span className='absolute right-3 top-1/4 transform-translate-y-1/2'><button type="button" onClick={handlePasswordToggle}>{showPassword==true ? <FaEye />:<FaEyeSlash />}</button></span>
             </div>
             <div className='w-full'>
-                <button type="submit" className='bg-indigo-700 rounded-md text-white pl-4 pr-4 pb-1 pt-1 flex justify-center mx-auto hover:bg-indigo-800'>Submit</button>
+                <button type="submit" className='bg-indigo-700 rounded-md text-white w-1/4 py-1 flex justify-center mx-auto hover:bg-indigo-800'>Submit</button>
             </div>
         </form>
         
