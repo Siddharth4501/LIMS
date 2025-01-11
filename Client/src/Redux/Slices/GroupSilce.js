3d45039a72380a2cfdbe5eeef2f9791a070f6be9
Siddharth Srivastava <143588718+Siddharth4501@users.noreply.github.com>
2025-01-11 23:18:52 +0530

    added group update and new group add function

--- Client/src/Redux/Slices/GroupSilce.js
+++ Client/src/Redux/Slices/GroupSilce.js
@@ -19,6 +19,46 @@ export const getGroupData = createAsyncThunk("group/data", async () => {
     }
   });
 
+  export const updateGroupData=createAsyncThunk("GroupData/Update",async(data)=>{
+    try{
+      let res=axios.put("http://localhost:5001/api/v1/Group/update",data)
+      await toast.promise(res, {
+        loading: "Loading...",
+        success: (data) => {
+          return data?.data?.message;
+        },
+        error: "Failed to update data",
+      });
+      
+      // getting response resolved here
+      res = await res;//when promise is resolved it will give data
+      return res.data;//catched by action
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  
+    })
+
+  export const addGroupData=createAsyncThunk("GroupData/add",async(data)=>{
+    try{
+      let res=axios.post("http://localhost:5001/api/v1/Group/add",data)
+      await toast.promise(res, {
+        loading: "Loading...",
+        success: (data) => {
+          return data?.data?.message;
+        },
+        error: "Failed to add Group",
+      });
+      
+      // getting response resolved here
+      res = await res;//when promise is resolved it will give data
+      return res.data;//catched by action
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  
+    })
+
   const groupSlice=createSlice({
       name:'group',
       initialState,
