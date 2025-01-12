e84812a53b2c03f9658eee840c410164bd83d203
Prince Kushwaha <143611059+imprince-kushwaha@users.noreply.github.com>
2025-01-13 00:46:08 +0530

    Upadted AddGroup

--- Client/src/pages/Admin/GroupRelated/AddGroup.jsx
+++ Client/src/pages/Admin/GroupRelated/AddGroup.jsx
@@ -3,6 +3,7 @@ import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { addGroupData } from '../../../Redux/Slices/GroupSilce';
 import toast from 'react-hot-toast';
+import AdminCommonPanel from '../../../components/AdminCommonPanel';
 
 const AddGroup = () => {
     const navigate=useNavigate();
@@ -27,19 +28,17 @@ const AddGroup = () => {
     }
   return (
     <div>
-        <div className='text-4xl w-full text-center p-4 font-bold'>
-            Add Group
-        </div>
-        <br /><br /><br /><br /><br /><br /><br />
-        <form className='flex flex-col w-1/3 h-56 mx-auto bg-slate-500 gap-5 justify-center px-10 rounded-md border border-indigo-700 border-2' onSubmit={handleSumbit}>
+        <div>
+        <form className='flex flex-col w-1/3 h-56 mx-auto bg-slate-500 gap-5 justify-center px-10 rounded-md border-indigo-700 border-2' onSubmit={handleSumbit}>
             <div className='flex w-full'>
                 <label htmlFor="GroupName" className='text-lg w-1/5'>Name:<span className='text-red-600'>*</span></label>
-                <input type="text" placeholder='Enter Group Name' name="GroupName" className='w-4/5 h-8 rounded-3xl pl-4 border border-blue-700 border-2' />
+                <input type="text" placeholder='Enter Group Name' name="GroupName" className='w-4/5 h-8 rounded-3xl pl-4 border-blue-700 border-2' />
             </div>
             <div className='w-full flex justify-center'>
                 <button type="submit" className='px-6 py-1 text-white bg-indigo-700 rounded-md hover:bg-indigo-900'>Submit</button>
             </div>
         </form>
+        </div>
     </div>
   )
 }
