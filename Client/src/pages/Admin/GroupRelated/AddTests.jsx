64d477aaed014ab9d31a11409fca3b10db82d06e
Siddharth Srivastava <143588718+Siddharth4501@users.noreply.github.com>
2025-01-13 19:23:55 +0530

    added addmore option

--- Client/src/pages/Admin/GroupRelated/AddTests.jsx
+++ Client/src/pages/Admin/GroupRelated/AddTests.jsx
@@ -8,14 +8,23 @@ const AddTests = () => {
     const dispatch=useDispatch();
     const navigate=useNavigate();
     const { groupData } = useSelector((state) => state.group);
-    const [selectedGroup,setSelectedGroup]=useState('')
-     const [typeOfTests, setTypeOfTests] = useState([{}]);
+    const [selectedGroupID,setSelectedGroupID]=useState('')
+    const [typeOfTesting,setTypeOfTesting]=useState('')
+     const [typeOfTests, setTypeOfTests] = useState([{
+        Type_Of_Testing:'',
+        Test:''
+     }]);
         const handleAddMore = () => {
-            setTypeOfTestingFields([...typeOfTests,'']);
+            setTypeOfTests([...typeOfTests,{
+                Type_Of_Testing:'',
+                Test:''
+            }]);
         };
         const handleInputChange = (index, e) => {
             const newFields = [...typeOfTests];
-            newFields[index] = e.target.value;
+            console.log(newFields,"koi",index);
+            newFields[index].Type_Of_Testing=typeOfTesting;
+            newFields[index].Test = e.target.value;
             setTypeOfTests(newFields);
         };
     useEffect(() => {
@@ -28,23 +37,19 @@ const AddTests = () => {
         const {value} =e.target;
         const filteredGroup=groupData?.filter((item)=>item.Group_Name===value)
         console.log(filteredGroup)
-        setSelectedGroup(filteredGroup[0]._id);
+        setSelectedGroupID(filteredGroup[0]._id);
     }
     const handleSumbit=async(e)=>{
         e.preventDefault();
         e.stopPropagation();
         const formData=new FormData(e.target)
         const GroupName=formData.get("GroupName");
-        // const GroupID=formData.get("GroupID");
-        const TypeOfTesting=formData.get("TypeOfTesting");
-        const Tests=formData.get("Tests");
-        console.log(GroupName,TypeOfTesting,Tests,"testka");
+        console.log(GroupName,typeOfTesting,typeOfTests,"testka");
         const data={
             "Group_Name":GroupName,
-            // "GroupID":GroupID,
-            "GroupID":selectedGroup,
-            "TypeOfTesting":TypeOfTesting,
-            "Tests":Tests
+            "GroupID":selectedGroupID,
+            "TypeOfTesting":typeOfTesting,
+            "Tests":typeOfTests
         }
         const res=await dispatch(updateGroupData(data));
         if(res?.payload?.success){
@@ -58,13 +63,16 @@ const AddTests = () => {
 
   return (
     <div>
-        <div className='text-4xl w-full text-center p-4 font-bold'>Add Tests</div>
+        <div className='w-full flex border bg-gray-300 shadow-[0_0_6px_gray] border-gray-800 border-[3px] p-5'>
+            <div className='w-3/5 text-3xl font-bold pr-24'><span className='float-right'>Add Tests</span></div>
+            <div className='w-2/5'><button className='bg-indigo-700 px-4 py-1 text-white rounded-md float-right' onClick={()=>navigate('/Admin/Home')}>Back</button></div>
+        </div>
         <br /><br /><br />
         <div className='w-full'>
-            <form className='flex flex-col w-1/2 h-96 mx-auto bg-slate-500 gap-5 justify-center px-10 rounded-md border-indigo-700 border-2' onSubmit={handleSumbit}>
+            <form className='flex flex-col w-1/2 min-h-96 mx-auto bg-slate-400 shadow-[0_0_6px_black] gap-5 justify-center px-10 rounded-md border-slate-700 border-[3px]' onSubmit={handleSumbit}>
                 <div className='flex '>
-                    <label htmlFor="" className='w-1/3 text-lg font-semibold'>Group:</label>
-                    <select name="" id="" className='w-2/3 p-1 rounded-3xl' onChange={handleOnChange}>
+                    <label htmlFor="GroupName" className='w-1/3 text-lg font-semibold'>Group:</label>
+                    <select name="GroupName" id="" className='w-2/3 p-1 rounded-3xl' onChange={handleOnChange}>
                         <option value="">Choose Group</option>
                         {
                             groupData.map((item)=>{
@@ -75,14 +83,14 @@ const AddTests = () => {
                 </div>
                 <div className='flex'>
                     <label htmlFor="" className='w-1/3 text-lg font-semibold'>Group ID:</label>
-                    <input type="text" disabled className='w-2/3 p-1 rounded-3xl' value={selectedGroup} />
+                    <input type="text" disabled className='w-2/3 p-1 rounded-3xl' value={selectedGroupID} />
                 </div>
                 <div className='flex'>
                     <label htmlFor="TypeOfTesting" className='w-1/3 text-lg font-semibold'>Enter Type Of Testing:</label>
-                    <select name="TypeOfTesting" id="" className='w-2/3 p-1 rounded-3xl' onChange={handleOnChange}>
+                    <select name="TypeOfTesting" id="" className='w-2/3 p-1 rounded-3xl' onChange={(e)=>setTypeOfTesting(e.target.value)}>
                         <option value="">Choose Type Of Testing</option>
                         {
-                            groupData?.filter((item)=>item._id===selectedGroup).map((data)=>{
+                            groupData?.filter((item)=>item._id===selectedGroupID).map((data)=>{
                                 {console.log("hrhr")}
                                 return data.Type_Of_Testing.map((TOT)=>{
                                     {console.log("jykuui",TOT)}
@@ -96,17 +104,28 @@ const AddTests = () => {
                 <div className='flex'>
                     <label htmlFor="Tests" className='w-1/3 text-lg font-semibold'>Enter Tests:</label>
                     {/* <input type="text" name='Tests' className='w-2/3 p-1 rounded-3xl' /> */}
-                    {typeOfTests.map((field, index) => (
-                            <div key={`${field}-${index}`} className="mb-2">
+                    <div className='w-2/3'>
+                        {typeOfTests.map((field, index) => (
+                            <div key={`${field}-${index}`} className="mb-2  full">
                                 <input
                                     type="text"
                                     name="TypeOfTesting"
-                                    value={field}
+                                    value={field.Test}
                                     onChange={(e) => handleInputChange(index, e)}
-                                    className="w-2/3 p-1 rounded-3xl"
-                                />
-                            </div> 
-                        ))}
+                                    className="w-full p-1 rounded-3xl"
+                                /> 
+                            </div>    
+                            ))}
+                    </div>
+                </div>
+                <div className=" w-full ">
+                        <button
+                            type="button"
+                            onClick={handleAddMore}
+                            className="text-sm text-white bg-indigo-700 py-1 px-4 float-right rounded-md"
+                        >
+                            Add More
+                        </button>
                 </div>
                 <div className='w-full flex justify-center'>
                     <button type="submit" className='w-1/3 py-1 ml-2 text-center text-white bg-indigo-700 rounded-md'>Submit</button>
