365b9132ee7c5049265a5e00545311c42e690cca
Siddharth Srivastava <143588718+Siddharth4501@users.noreply.github.com>
2025-01-14 21:07:38 +0530

    added route to delete a sample

--- Server/routes/Sample.routes.js
+++ Server/routes/Sample.routes.js
@@ -1,11 +1,12 @@
 import {Router} from "express";
-import { SampleRegister,SampleData,SampleEdit,TMDataSave, TMANData,TMANDataUpdate } from "../controllers/Sample.controller.js";
+import { SampleRegister,SampleData,SampleEdit,TMDataSave, TMANData,TMANDataUpdate, DeleteSampleData } from "../controllers/Sample.controller.js";
 const router=Router()
 
 router.post('/register',SampleRegister)
 router.post('/TM/data/save',TMDataSave)
 router.get('/TMAN/data',TMANData)
 router.get('/data',SampleData)
+router.post('/delete',DeleteSampleData)
 router.put('/edit',SampleEdit)
 router.put('/TMAN/data/update',TMANDataUpdate)
 export default router;
