bb46876815064ab35d97997616ed2f60e2e795e1
Siddharth Srivastava <143588718+Siddharth4501@users.noreply.github.com>
2025-01-15 02:03:44 +0530

    added conditions for various redirections

--- Client/src/components/Samples.jsx
+++ Client/src/components/Samples.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import {  BsTrash } from "react-icons/bs";
+import { DeleteSampleData } from '../Redux/Slices/SampleSlice';
+import toast from 'react-hot-toast';
+import { useDispatch } from 'react-redux';
 
 const Samples = ({difference,data,index}) => {
+  const dispatch=useDispatch();
     const navigate=useNavigate();
     const handleRedirection=()=>{
         if(difference==='All Sample History'){
@@ -11,11 +15,29 @@ const Samples = ({difference,data,index}) => {
         else if(difference==='ParticularUser Sample History'){
           navigate('/SampleRegistrationUser/SampleHistory/View_More',{state:{...data,difference}})
         }
+        else if(difference==='All Deleted Sample History'){
+          navigate('/Admin/Sample/DeletedSampleHistory/View_More',{state:{...data,difference}})
+        }
         else{
           navigate('/SampleAllotment/View_More',{state:{...data}})
         }
         
       }
+    const handleDelete=async(sampleID)=>{
+      try {
+        console.log(sampleID,"judju")
+        const data={
+          "sampleID":sampleID
+        }
+        const response = await dispatch(DeleteSampleData(data));
+        if (response?.payload?.success) {
+          toast.success('Sample Deleted Successfully');
+          navigate('/Admin/Home')
+        }
+      } catch (error) {
+          toast.error(error)
+      }
+    }
   return (
           
           <tbody>
@@ -29,12 +51,19 @@ const Samples = ({difference,data,index}) => {
                 <button className="bg-indigo-700 hover:bg-indigo-900 rounded-md text-white pl-4 pr-4 pt-1 pb-1" onClick={handleRedirection}>View </button>
               </td>
               {
-                difference==='All Sample History' || difference=== 'ParticularUser Sample History' ?(
-                  <td className="border border-gray-300 px-4 py-2 text-center">                 
-                    <button className="bg-red-700 hover:bg-red-800 rounded-md text-white pl-4 pr-4 pt-1 pb-1"><BsTrash/></button>
-                  </td>
+                difference==='All Sample History' ?(
+                  <>
+                    <td className='border border-gray-300 px-4 py-2 text-center'>
+                      {data.Sample_Status}
+                    </td>
+                    <td className="border border-gray-300 px-4 py-2 text-center">                 
+                    {data?.Sample_Status==='Forwarded To TM'?<button className="bg-red-700 hover:bg-red-800 rounded-md text-white pl-4 pr-4 pt-1 pb-1" onClick={()=>handleDelete(data._id)}><BsTrash/></button>:<button className="bg-red-700 rounded-md text-white pl-4 pr-4 pt-1 pb-1 cursor-not-allowed" disabled={true}><BsTrash/></button>}
+                    </td>
+                  </>
+                ):difference=== 'ParticularUser Sample History'?(
+                    data.Active===true?<td className='border border-gray-300 px-4 py-2 text-center'><span className='bg-green-600 text-white px-6 py-1 rounded-md'>Active</span></td>:<td className='border border-gray-300 px-4 py-2 text-center'><span className='bg-red-600 text-white px-4 py-1 rounded-md'>Deleted</span></td> 
                 ):(
-                  <span className='w-0'></span>
+                  <td className='w-0'></td>
                 )
               }
             </tr>
