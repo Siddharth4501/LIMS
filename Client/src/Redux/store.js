8fee451193abfad6ac7edf7a31ed2823681e71ed
Siddharth Srivastava <143588718+Siddharth4501@users.noreply.github.com>
2025-01-17 20:00:59 +0530

    added substance reducer to store

--- Client/src/Redux/store.js
+++ Client/src/Redux/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import authSliceReducer from './Slices/AuthSlice.js'
 import groupSliceReducer from './Slices/GroupSilce.js'
 import sampleSliceReducer from './Slices/SampleSlice.js'
+import substanceSliceReducer from "./Slices/SubstanceSlice.js";
 
 const store=configureStore({
     reducer:{
         auth:authSliceReducer,
         group:groupSliceReducer,
-        sample:sampleSliceReducer
+        sample:sampleSliceReducer,
+        substance:substanceSliceReducer
     },
     devtools:true
 });
