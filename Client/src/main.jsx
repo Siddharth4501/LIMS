3a1aa2e46850a6bb4929203caaca39cd99f0d096
Siddharth Srivastava <143588718+Siddharth4501@users.noreply.github.com>
2025-01-19 23:31:23 +0530

    added BrowserRouter

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -4,10 +4,13 @@ import App from './App.jsx'
 import { Toaster } from 'react-hot-toast';
 import { Provider } from 'react-redux';
 import store from './Redux/store.js';
+import {BrowserRouter} from 'react-router-dom';
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <App />
-    <Toaster/>
+    <BrowserRouter>
+      <App />
+      <Toaster/>
+    </BrowserRouter>
   </Provider>
   ,
 )
