0a85041e36a78089b59aee33a6c95d59c860f347
Siddharth Srivastava <143588718+Siddharth4501@users.noreply.github.com>
2025-01-20 23:11:31 +0530

    added excel sheet upload button

--- Client/src/pages/Analyst/AN_PendingSamplesViewMore.jsx
+++ Client/src/pages/Analyst/AN_PendingSamplesViewMore.jsx
@@ -3,6 +3,7 @@ import { useDispatch } from 'react-redux';
 import { useLocation, useNavigate } from 'react-router-dom'
 import { updateTMANData } from '../../Redux/Slices/SampleSlice';
 import toast from 'react-hot-toast';
+import * as XLSX from "xlsx";
 
 const AN_PendingSamplesViewMore = () => {
     const {state}=useLocation();
@@ -11,10 +12,19 @@ const AN_PendingSamplesViewMore = () => {
     const userData=JSON.parse(localStorage.getItem("userData"));
     console.log(state,state.TMANID,"hjtyj");
     const [substances,setSubstance]=useState(state.Substances_To_Be_Analysed)
+    const [resultColumn, setResultColumn] = useState([]);
     console.log(substances,"opin");
 
-    const handleResultChange=(e,typeOfTesting,testID)=>{
+    const handleResultChange=(e,typeOfTesting,testID,index)=>{
         const { value } = e.target;
+        if(resultColumn.length>0){
+            setResultColumn((prev) => {
+                return prev.map((item, idx) =>
+                  idx === index ? { ...item, Result: value } : item
+                );
+              });
+        }
+        console.log(setResultColumn,"hkfw")
         setSubstance((prevState) => ({
             ...prevState,
             [typeOfTesting]:{
@@ -70,11 +80,42 @@ const AN_PendingSamplesViewMore = () => {
           toast.error("Something Went Wrong");
         }
     }
+    const handleExcelFileUpload=(e)=>{
+        const file = e.target.files[0]; // Get the uploaded file
+    if (file) {
+      const reader = new FileReader();
+
+      reader.onload = (event) => {
+        const data = new Uint8Array(event.target.result);
+        const workbook = XLSX.read(data, { type: "array" });
+        const sheetName = workbook.SheetNames[0]; // Get the first sheet
+        const worksheet = workbook.Sheets[sheetName];
+        const jsonData = XLSX.utils.sheet_to_json(worksheet); // Convert sheet to JSON
+        const extractedData=[]
+        // Extract the "Result" column
+        jsonData.map((row) => {
+            const obj={
+            Name: row["Name"],    // Assuming the "Name" column exists
+            Result: row["Result"], // Assuming the "Result" column exists
+          }
+          extractedData.push(obj);
+        }); // Assuming "Result" is the column name
+        setResultColumn(extractedData);
+      };
+
+      reader.readAsArrayBuffer(file); // Read the file as a binary array
+    }
+  };
+  console.log(resultColumn,"yup")
   return (
       <form onSubmit={handleSubmit}>
         <div>
           <div className='flex border bg-gray-300 text-3xl justify-center font-bold p-5'>
               Add Result Page
+          </div>
+          <br /><br />
+          <div className='w-full'>
+            <input type="file" accept=".xlsx,.xls" name="ExcelFileInput" id="ExcelFileInput" onChange={handleExcelFileUpload} className='float-right bg-slate-200 p-1'/>
           </div>
           <br /><br />
           {
@@ -104,19 +145,32 @@ const AN_PendingSamplesViewMore = () => {
                                       </thead>
                                       {
                                           state.Substances_To_Be_Analysed[key].Tests.filter((data)=>data.Analyst.ID===userData._id).map((item, index) => {
+                                            {console.log(index,resultColumn[index],"liop")}
                                               return (
+                                                
                                                   <tbody key={`${key}-${index}`}>
-    
-                                                      <tr className="hover:bg-gray-100">
-                                                          <td className="border border-gray-300 px-4 py-2 text-center font-bold">{index + 1}.</td>
-                                                          <td className="border border-gray-300 px-4 py-2 text-center">{item.Test.Test_Name}</td>
-                                                          <td className="border border-gray-300 px-4 py-2 text-center">{item.Method}</td>
-                                                          <td className="border border-gray-300 px-4 py-2 text-center">{item.Unit}</td>
-                                                          <td className="border border-gray-300 px-4 py-2 text-center"><input type="text" name={`Result-${item.Test.Test_Name}`} id={`Result-${item.Test.Test_Name}`} defaultValue={0} min={0} required onChange={(e)=>handleResultChange(e,key,item.Test.TestID)} className='text-center bg-zinc-300 rounded-md min-w-8' /></td>
-                                                          <td className="border border-gray-300 px-4 py-2 text-center">
-                                                          <input type="date" name={`Start_Date-${key}`} id={`Start_Date-${key}`} className='bg-zinc-300 p-1 mb-2 rounded' onChange={(e)=>handleStartDate(e,key,item.Test.TestID)}/>-<input type="date" name={`End_Date-${key}`} id={`Start_Date-${key}`} className='bg-zinc-300 p-1 mb-2 rounded' onChange={(e)=>handleEndDate(e,key,item.Test.TestID)}/>
-                                                          </td>
-                                                      </tr>
+                                                    <tr className="hover:bg-gray-100">
+                                                        <td className="border border-gray-300 px-4 py-2 text-center font-bold">{index + 1}.</td>
+                                                        <td className="border border-gray-300 px-4 py-2 text-center">{item.Test.Test_Name}</td>
+                                                        <td className="border border-gray-300 px-4 py-2 text-center">{item.Method}</td>
+                                                        <td className="border border-gray-300 px-4 py-2 text-center">{item.Unit}</td>
+                                                
+                                                        <td className="border border-gray-300 px-4 py-2 text-center"><input type="text" name={`Result-${item.Test.Test_Name}`} id={`Result-${item.Test.Test_Name}`} 
+                                                        value={
+                                                            resultColumn.length > 0 &&
+                                                            resultColumn[index] &&
+                                                            resultColumn[index].Name.trim() === item.Test.Test_Name.trim()
+                                                                ? resultColumn[index].Result
+                                                                : "0"
+                                                            } required onChange={(e)=>handleResultChange(e,key,item.Test.TestID,index)} className='text-center bg-zinc-300 rounded-md min-w-8 p-1' />
+                                                        </td>
+                                                            
+                                                        
+                                                        
+                                                        <td className="border border-gray-300 px-4 py-2 text-center">
+                                                        <input type="date" name={`Start_Date-${key}`} id={`Start_Date-${key}`} className='bg-zinc-300 p-1 m-2 rounded' onChange={(e)=>handleStartDate(e,key,item.Test.TestID)}/>-<input type="date" name={`End_Date-${key}`} id={`Start_Date-${key}`} className='bg-zinc-300 p-1 mb-2 rounded' onChange={(e)=>handleEndDate(e,key,item.Test.TestID)}/>
+                                                        </td>
+                                                    </tr>
                                                   </tbody>
                                               )
                                           })}
