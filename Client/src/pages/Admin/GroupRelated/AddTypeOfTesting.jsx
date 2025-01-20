49ecf0a300a836454a9a93f99dccde83e00f664e
Prince Kushwaha <143611059+imprince-kushwaha@users.noreply.github.com>
2025-01-20 12:50:14 +0530

    Added AdminCommomNav to AddTypeofTesting

--- Client/src/pages/Admin/GroupRelated/AddTypeOfTesting.jsx
+++ Client/src/pages/Admin/GroupRelated/AddTypeOfTesting.jsx
@@ -63,11 +63,9 @@ const AddTypeOfTesting = () => {
 
   return (
     <div>
-        <div className='w-full flex border bg-gray-300 shadow-[0_0_6px_gray] border-gray-800 border-[3px] p-5'>
-            <div className='w-3/5 text-3xl font-bold'><span className='float-right'>Add Type Of Testing</span></div>
-            <div className='w-2/5'><button className='bg-indigo-700 px-4 py-1 text-white rounded-md float-right' onClick={()=>navigate('/Admin/Group/TypeOfTestingList')}>Back</button></div>
-        </div>
-        <br /><br /><br /><br /><br />
+        <AdminCommomNav/>
+            <div className='w-full p-4'><button className='bg-indigo-700 px-8 py-1 text-white rounded-md float-right' onClick={()=>navigate('/Admin/Group/TypeOfTestingList')}>Back</button></div>
+        <br /><br /><br />
         <div className='w-full'>
             <form className='flex flex-col w-1/2 min-h-96 mx-auto bg-slate-400 gap-5 shadow-[0_0_6px_black] justify-center px-10 rounded-md border-slate-700 border-[3px]' onSubmit={handleSumbit}>
                 <div className='flex'>
