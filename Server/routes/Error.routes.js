dbbdf5237c5a43ee14c1a0da661057947b621cc0
Siddharth Srivastava <143588718+Siddharth4501@users.noreply.github.com>
2025-01-21 18:05:36 +0530

    added routes for Error

--- Server/routes/Error.routes.js
+++ Server/routes/Error.routes.js
@@ -1,5 +1,5 @@
 import {Router} from "express";
-import { ErrorAdd, FetchError } from "../controllers/Error.controller";
+import { ErrorAdd, FetchError } from "../controllers/Error.controller.js";
 
 const router=Router()
 
