da2ff959aaad456c6725789014d1308570849085
Siddharth Srivastava <143588718+Siddharth4501@users.noreply.github.com>
2025-01-21 18:04:27 +0530

    added Error route in app.js

--- Server/app.js
+++ Server/app.js
@@ -6,6 +6,7 @@ import sampleRoutes from './routes/Sample.routes.js'
 import userRoutes from './routes/user.routes.js'
 import substanceRoutes from './routes/Substance.routes.js'
 import groupRoutes from './routes/Group.routes.js'
+import errorRoutes from './routes/Error.routes.js'
 import errorMiddleware from './middlewares/error.middleware.js';
 
 const app=express();
@@ -26,6 +27,7 @@ app.use('/api/v1/Sample',sampleRoutes);
 app.use('/api/v1/user',userRoutes);
 app.use('/api/v1/Substance',substanceRoutes);
 app.use('/api/v1/Group',groupRoutes);
+app.use('/api/v1/Error',errorRoutes);
 
 app.all('*',(req,res)=>{
     res.status(404).send('OOPS!! 404 Page Not Found');
