c4e3378eb108606570b52df99c29a8b848e3aa2c
Siddharth Srivastava <143588718+Siddharth4501@users.noreply.github.com>
2025-01-22 23:21:54 +0530

    added routes to delete Group,Type Of Testing,Test

--- Server/routes/Group.routes.js
+++ Server/routes/Group.routes.js
@@ -1,9 +1,12 @@
 import {Router} from "express";
-import { GroupAdd, GroupData, GroupUpdate } from "../controllers/Group.controller.js";
+import { GroupAdd, GroupData, GroupDelete, GroupUpdate,TestsDelete,TypeOfTestingDelete } from "../controllers/Group.controller.js";
 
 const router=Router()
 
 router.post('/add',GroupAdd)
 router.get('/data',GroupData)
 router.put('/update',GroupUpdate)
+router.delete('/delete/:id',GroupDelete)
+router.post('/TypeOfTesting/delete',TypeOfTestingDelete)
+router.post('/Test/delete',TestsDelete)
 export default router;
