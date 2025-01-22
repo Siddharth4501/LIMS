6df0913ac2756b876a2e254fad26a0ca7093aecd
Siddharth Srivastava <143588718+Siddharth4501@users.noreply.github.com>
2025-01-22 23:32:22 +0530

    changed css

--- Client/src/pages/Admin/MainUI/MainUI.jsx
+++ Client/src/pages/Admin/MainUI/MainUI.jsx
@@ -15,7 +15,7 @@ const MainUI = () => {
                 <AdminCommonPanel />
 
                 {/* Right Section */}
-                <div className='w-full bg-[url("/src/assets/images/AdminBack4.jpg")] bg-cover bg-no-repeat bg-bottom' style={{ backgroundColor: "rgba(0, 0, 0, 0.2)",backgroundBlendMode: "darken"}}>
+                <div className='w-full bg-[url("/src/assets/images/AdminBack4.jpg")] bg-cover bg-no-repeat bg-bottom' style={{ backgroundColor: "rgba(0, 0, 0, 0.1)",backgroundBlendMode: "darken"}}>
                 </div>
             </div>
         </div>
