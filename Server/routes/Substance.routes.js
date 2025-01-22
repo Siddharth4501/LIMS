d8bd527fe3e228bc3b6afd8c38824c34c0d5225c
Siddharth Srivastava <143588718+Siddharth4501@users.noreply.github.com>
2025-01-22 23:21:24 +0530

    added route to delete substance

--- Server/routes/Substance.routes.js
+++ Server/routes/Substance.routes.js
@@ -1,8 +1,9 @@
 import {Router} from "express"
-import { SubstanceAdd, SubstanceData,SubstanceEdit } from "../controllers/Substance.controller.js"
+import { SubstanceAdd, SubstanceData,SubstanceDelete,SubstanceEdit } from "../controllers/Substance.controller.js"
 const router=Router()
 
 router.post('/add',SubstanceAdd)
 router.get('/data',SubstanceData)
 router.get('/edit',SubstanceEdit)
+router.delete('/delete/:id',SubstanceDelete)
 export default router;
