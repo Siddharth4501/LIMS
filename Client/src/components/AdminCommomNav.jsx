d877f937342fec6ec5a15c664a40512211ebd1a1
Siddharth Srivastava <143588718+Siddharth4501@users.noreply.github.com>
2025-01-22 23:25:25 +0530

    added navigation function to navigate to home from anywhere

--- Client/src/components/AdminCommomNav.jsx
+++ Client/src/components/AdminCommomNav.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const AdminCommomNav = () => {
+    const navigate=useNavigate();
+    const handleAdminNavigation=(e)=>{
+        const {value}=e.target;
+        console.log(value,"frt")
+        if(value==="AdminHome"){
+            navigate('/Admin/Home');
+        }
+    }
   return (
       <div className="flex items-center justify-between p-4 shadow-md bg-slate-400 border-slate-700 border-2">
                 <div className="flex items-center">
@@ -10,8 +19,9 @@ const AdminCommomNav = () => {
                     <span className="text-lg font-bold">Name of Lab : DFRL</span>
                 </div>
                 <div className='mx-6'>
-                    <select name="" id="" className="p-2 rounded-lg border border-blue-800 bg-slate-100">
+                    <select name="" id="" className="p-2 rounded-lg border border-blue-800 bg-slate-100" onChange={handleAdminNavigation}>
                         <option value="admin">Administrator</option>
+                        <option value="AdminHome">Home</option>
                         <option value="change-password">Change Password</option>
                         <option value="logout" className='text-red-500'>Log Out!</option>
                     </select>
