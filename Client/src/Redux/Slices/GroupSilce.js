cc1d93cec367ac33a023b4a9278f649ad5210f0b
Siddharth Srivastava <143588718+Siddharth4501@users.noreply.github.com>
2025-01-22 23:24:40 +0530

    added functions to  send request to delete Group,Type Of Testing,Test at backend

--- Client/src/Redux/Slices/GroupSilce.js
+++ Client/src/Redux/Slices/GroupSilce.js
@@ -43,6 +43,73 @@ export const getGroupData = createAsyncThunk("group/data", async () => {
   
     })
 
+  export const deleteGroupData=createAsyncThunk("GroupData/Delete",async(data)=>{
+    try{
+      const {groupID}=data
+      const id=groupID;
+      let res=axios.delete(`http://localhost:5001/api/v1/Group/delete/${id}`,{
+        withCredentials: true, // Include cookies
+      })
+      await toast.promise(res, {
+        loading: "Loading...",
+        success: (data) => {
+          return data?.data?.message;
+        },
+        error: "Failed to delete data",
+      });
+      
+      // getting response resolved here
+      res = await res;//when promise is resolved it will give data
+      return res.data;//catched by action
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  
+    })
+    export const deleteTypeOfTesting=createAsyncThunk("TypeOfTesting/Delete",async(data)=>{
+      try{
+        let res=axios.post("http://localhost:5001/api/v1/Group/TypeOfTesting/delete",data,{
+          withCredentials: true, // Include cookies
+        })
+        await toast.promise(res, {
+          loading: "Loading...",
+          success: (data) => {
+            return data?.data?.message;
+          },
+          error: "Failed to Delete Type Of Testing",
+        });
+        
+        // getting response resolved here
+        res = await res;//when promise is resolved it will give data
+        return res.data;//catched by action
+      } catch (error) {
+        toast.error(error.response.data.message);
+      }
+    
+        })
+      
+        export const deleteTest=createAsyncThunk("Test/Delete",async(data)=>{
+          try{
+            let res=axios.post("http://localhost:5001/api/v1/Group/Test/delete",data,{
+              withCredentials: true, // Include cookies
+            })
+            await toast.promise(res, {
+              loading: "Loading...",
+              success: (data) => {
+                return data?.data?.message;
+              },
+              error: "Failed to Delete Test",
+            });
+            
+            // getting response resolved here
+            res = await res;//when promise is resolved it will give data
+            return res.data;//catched by action
+          } catch (error) {
+            toast.error(error.response.data.message);
+          }
+        
+            })
+
   export const addGroupData=createAsyncThunk("GroupData/add",async(data)=>{
     try{
       let res=axios.post("http://localhost:5001/api/v1/Group/add",data,{
