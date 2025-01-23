1a2826bce8a433fb07e40cb5242070649e9b9bfd
Siddharth Srivastava <143588718+Siddharth4501@users.noreply.github.com>
2025-01-23 23:28:50 +0530

    added route to delete user role

--- Server/routes/user.routes.js
+++ Server/routes/user.routes.js
@@ -1,5 +1,5 @@
 import {Router} from "express";
-import { Login,Register,Logout, UserData, changePassword,DeleteUserData } from "../controllers/user.controller.js";
+import { Login,Register,Logout, UserData, changePassword,DeleteUserData,DeleteUserRole } from "../controllers/user.controller.js";
 import { isLoggedIn } from "../middlewares/auth.middleware.js";
 
 const router=Router()
@@ -8,6 +8,7 @@ router.post('/register',Register)
 router.post('/login',Login)
 router.post('/Change-Password',isLoggedIn,changePassword)
 router.post('/delete',DeleteUserData)
+router.post('/role/delete',DeleteUserRole)
 router.get('/data',UserData)
 router.get('/logout',Logout)
 export default router
