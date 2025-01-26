0512ea6c4185ece98d3e158161df12f101a8e70a
Siddharth Srivastava <143588718+Siddharth4501@users.noreply.github.com>
2025-01-26 20:47:28 +0530

    added name of lab page

---
+++ Client/src/pages/Admin/ExtraDetailsRelated/NameOfLab.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import AdminCommomNav from '../../../components/AdminCommomNav'
+import AdminCommonPanel from '../../../components/AdminCommonPanel'
+
+const NameOfLab = () => {
+  return (
+    <>
+          <AdminCommomNav />
+          <div className='flex'>
+              <AdminCommonPanel/>
+              <div className='w-full'>
+
+              </div>
+          </div>
+    </>
+  )
+}
+
+export default NameOfLab
