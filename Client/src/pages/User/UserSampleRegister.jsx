9636f8f15e5db721b2fcd9ee4b2a8e26bf8144f6
Prince Kushwaha <143611059+imprince-kushwaha@users.noreply.github.com>
2025-01-28 00:39:32 +0530

    Updated UI of UserSampleRegister

--- Client/src/pages/User/UserSampleRegister.jsx
+++ Client/src/pages/User/UserSampleRegister.jsx
@@ -1,43 +1,43 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
-import {useDispatch} from 'react-redux';
+import { useDispatch } from 'react-redux';
 import toast from 'react-hot-toast';
 import { logout } from '../../Redux/Slices/AuthSlice';
 
 const UserSampleRegister = () => {
-  const dispatch=useDispatch();
-  const navigate=useNavigate();
-  const userData=JSON.parse(localStorage.getItem('userData'));
-    const handleSampleHistoryPage=()=>{
-        navigate('/SampleRegistrationUser/SampleHistory')
-    }
-    const handleSampleRegister=()=>{
-      navigate('/SampleRegister')
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const userData = JSON.parse(localStorage.getItem('userData'));
+  const handleSampleHistoryPage = () => {
+    navigate('/SampleRegistrationUser/SampleHistory')
   }
-  const handleRedirection=async(e)=>{
-    const value=e.target.value;
-    if(value==='User Home'){
-        navigate('/')
+  const handleSampleRegister = () => {
+    navigate('/SampleRegister')
+  }
+  const handleRedirection = async (e) => {
+    const value = e.target.value;
+    if (value === 'User Home') {
+      navigate('/')
     }
-    else if(value==='logout'){
-      const res=await dispatch(logout())
-      if(res?.payload?.success){
+    else if (value === 'logout') {
+      const res = await dispatch(logout())
+      if (res?.payload?.success) {
         navigate('/Login')
         toast.success("Successfully Logged Out");
-          
+
       }
-      else{
-          toast.error("Something Went Wrong");
+      else {
+        toast.error("Something Went Wrong");
       }
     }
-    else if(value=='change-password'){
+    else if (value == 'change-password') {
       navigate('/User/Change-Password')
     }
   }
   return (
     <>
-    <div>
-    </div>
+      <div>
+      </div>
       <div className="flex items-center justify-between p-4 shadow-md bg-slate-400 border border-2 border-slate-700">
         <div className="flex items-center">
           <img src="/src/assets/images/DRDO-Logo1.jpg" alt="Logo"
@@ -46,7 +46,7 @@ const UserSampleRegister = () => {
           <span className="text-lg font-bold">Name of Lab: DFRL</span>
         </div>
         <div>
-        <h1 className='text-center font-medium text-3xl p-4'>Sample Registration Page</h1>
+          <h1 className='text-center font-medium text-3xl p-4'>Sample Registration Page</h1>
         </div>
         <div className='mx-6'>
           <select name="" id="" className="p-2 rounded-lg border border-blue-800 bg-slate-100" onChange={handleRedirection}>
@@ -61,20 +61,14 @@ const UserSampleRegister = () => {
       <br /><br /><br /><br /><br /><br /><br />
       <div className='grid grid-cols-2 gap-12 mt-16 px-4 py-2 m-auto w-3/4'>
         <center>
-            <button className='bg-red-500  shadow-lg shadow-red-500/50 w-80 px-4 py-2 rounded-md ml-8 text-2xl font-normal border-black border-2 transition-transform duration-300 ease-in-out hover:scale-105' onClick={handleSampleRegister}>
-                <div className='flex flex-col gap-3'>
-                    <div className='h-32 w-32 rounded-full border-2 border-white bg-red-800 text-white flex justify-center mx-auto '><div className='flex justify-center m-auto font-bold text-2xl'>SR</div></div>
-                    <div className='text-white'>Sample Register</div>
-                </div>
-            </button>
+          <button className='bg-red-500 shadow-lg shadow-red-500/50 w-80 px-4 py-2 rounded-md text-2xl font-normal transition-transform duration-300 ease-in-out hover:scale-105' onClick={handleSampleRegister}>
+            Sample Register
+          </button>
         </center>
         <center>
-            <button className='bg-cyan-500  shadow-lg shadow-cyan-500/50 w-80 px-4 py-2 rounded-md mr-32 text-2xl font-normal border-black border-2 transition-transform duration-300 ease-in-out hover:scale-105' onClick={handleSampleHistoryPage}>
-                <div className='flex flex-col gap-3'>
-                    <div className='h-32 w-32 rounded-full border-2 border-white bg-cyan-800 text-white flex justify-center mx-auto '><div className='flex justify-center m-auto font-bold text-2xl'>SH</div></div>
-                    <div className='text-white'>Sample History</div>
-                </div>
-            </button>
+          <button className='bg-cyan-500 shadow-lg shadow-cyan-500/50 w-80 px-4 py-2 rounded-md text-2xl font-normal transition-transform duration-300 ease-in-out hover:scale-105' onClick={handleSampleHistoryPage}>
+            Sample History Page
+          </button>
         </center>
       </div>
     </>
