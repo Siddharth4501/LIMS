540dd99b3186742753aa2244774466c73e270493
Prince Kushwaha <143611059+imprince-kushwaha@users.noreply.github.com>
2025-01-28 00:39:57 +0530

    Updated UI of AnalystHome

--- Client/src/pages/Analyst/AnalystHome.jsx
+++ Client/src/pages/Analyst/AnalystHome.jsx
@@ -6,37 +6,37 @@ import { logout } from '../../Redux/Slices/AuthSlice';
 
 const AnalystHome = () => {
   const navigate = useNavigate();
-  const dispatch=useDispatch();
-  const userData=JSON.parse(localStorage.getItem('userData'));
+  const dispatch = useDispatch();
+  const userData = JSON.parse(localStorage.getItem('userData'));
   const handlePendingSample = () => {
     navigate('/AN_PendingSamples')
   }
   const handleCompletedSample = () => {
     navigate('/AN_CompletedSamples')
   }
-  const handleRedirection=async(e)=>{
-    const value=e.target.value;
-    if(value==='User Home'){
-        navigate('/')
-    }  
-    else if(value==='logout'){
-      const res=await dispatch(logout())
-      if(res?.payload?.success){
+  const handleRedirection = async (e) => {
+    const value = e.target.value;
+    if (value === 'User Home') {
+      navigate('/')
+    }
+    else if (value === 'logout') {
+      const res = await dispatch(logout())
+      if (res?.payload?.success) {
         navigate('/Login')
         toast.success("Successfully Logged Out");
-          
+
       }
-      else{
-          toast.error("Something Went Wrong");
+      else {
+        toast.error("Something Went Wrong");
       }
     }
-    else if(value=='change-password'){
+    else if (value == 'change-password') {
       navigate('/User/Change-Password')
     }
-}
+  }
   return (
     <>
-      
+
       <div className="flex items-center justify-between p-4 shadow-md bg-slate-400 border border-2 border-slate-700">
 
         <div className="flex items-center">
@@ -58,20 +58,14 @@ const AnalystHome = () => {
       <br /><br /><br /><br /><br /><br />
       <div className='grid grid-cols-2 gap-12 mt-16 px-4 py-2 m-auto w-3/4'>
         <center>
-            <button className='bg-red-500  shadow-lg shadow-red-500/50 w-80 px-4 py-2 rounded-md ml-8 text-2xl font-normal border-black border-2 transition-transform duration-300 ease-in-out hover:scale-105' onClick={handlePendingSample}>
-                <div className='flex flex-col gap-3'>
-                    <div className='h-32 w-32 rounded-full border-2 border-white bg-red-800 text-white flex justify-center mx-auto '><div className='flex justify-center m-auto font-bold text-2xl'>PS</div></div>
-                    <div className='text-white'>Pending Samples</div>
-                </div>
-            </button>
+          <button className='bg-red-500 shadow-lg shadow-red-500/50 w-80 px-4 py-2 rounded-md text-2xl font-normal transition-transform duration-300 ease-in-out hover:scale-105' onClick={handlePendingSample}>
+            Pending Samples
+          </button>
         </center>
         <center>
-            <button className='bg-cyan-500  shadow-lg shadow-cyan-500/50 w-80 px-4 py-2 rounded-md mr-32 text-2xl font-normal border-black border-2 transition-transform duration-300 ease-in-out hover:scale-105' onClick={handleCompletedSample}>
-                <div className='flex flex-col gap-3'>
-                    <div className='h-32 w-32 rounded-full border-2 border-white bg-cyan-800 text-white flex justify-center mx-auto '><div className='flex justify-center m-auto font-bold text-2xl'>CS</div></div>
-                    <div className='text-white'>Completed Samples </div>
-                </div>
-            </button>
+          <button className='bg-cyan-500 shadow-lg shadow-cyan-500/50 w-80 px-4 py-2 rounded-md text-2xl font-normal transition-transform duration-300 ease-in-out hover:scale-105' onClick={handleCompletedSample}>
+            Completed Samples
+          </button>
         </center>
       </div>
     </>
