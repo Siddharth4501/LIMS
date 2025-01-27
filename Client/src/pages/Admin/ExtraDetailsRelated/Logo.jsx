f74d1d7b907a6ffa368e46933f3af543e1c53eb9
Siddharth Srivastava <143588718+Siddharth4501@users.noreply.github.com>
2025-01-27 21:07:13 +0530

    worked on logo page

--- Client/src/pages/Admin/ExtraDetailsRelated/Logo.jsx
+++ Client/src/pages/Admin/ExtraDetailsRelated/Logo.jsx
@@ -4,15 +4,20 @@ import AdminCommonPanel from '../../../components/AdminCommonPanel'
 
 const Logo = () => {
   return (
-      <>
-          <AdminCommomNav />
-          <div className='flex'>
-              <AdminCommonPanel/>
-              <div className='w-full'>
-
-              </div>
-          </div>
-      </>
+    <>
+        <AdminCommomNav />
+        <div className='flex'>
+            <AdminCommonPanel/>
+            <div className='w-full'>
+            <br /><br /><br /><br /><br />
+                <div className='flex flex-col gap-2 w-1/2 justify-center m-auto h-72 border-2 bg-slate-300 border-2 border-blue-700 items-center'>
+                    <div className='text-xl font-semibold'>Logo:</div>
+                    <div className='w-full'><input type="file" placeholder='Choose Logo' className='w-3/4 flex justify-center mx-auto h-12 p-2 border-2 bg-slate-100 border-blue-600 font-semibold outline-0' /></div>
+                    <div><button className='px-4 py-1 bg-indigo-700 hover:bg-indigo-900 text-white rounded-md mt-3'>Submit</button></div>
+                </div>
+            </div>
+        </div>
+    </>
   )
 }
 
