bb924f5f781f4a4acc21530cf2c94255e19aacf1
Siddharth Srivastava <143588718+Siddharth4501@users.noreply.github.com>
2025-01-28 20:49:49 +0530

    worked on Lab Name page

--- Client/src/pages/Admin/ExtraDetailsRelated/NameOfLab.jsx
+++ Client/src/pages/Admin/ExtraDetailsRelated/NameOfLab.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AdminCommomNav from '../../../components/AdminCommomNav'
 import AdminCommonPanel from '../../../components/AdminCommonPanel'
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import toast from 'react-hot-toast'
+import { saveNameOfLab } from '../../../Redux/Slices/ExtraSlice'
 
 const NameOfLab = () => {
+  const dispatch=useDispatch();
+  const navigate=useNavigate();
+  const [LabName,setLabName]=useState();
+  const handleSubmit=async()=>{
+    if(!LabName){
+      toast.error("Lab Name is required");
+      return
+    }
+    const data={
+      "Lab_Name":LabName,
+    }
+
+    try{
+      const res=await dispatch(saveNameOfLab(data));
+        if(res?.payload?.success){
+            toast.success("Name Of Lab Saved Successfully");
+            navigate('/Admin/Home');
+        }
+        else{
+            toast.error("Something went Wrong");
+        }
+    }
+    catch(e){
+      toast.error("Something Went Wrong");
+    }
+  }
   return (
     <>
           <AdminCommomNav />
@@ -12,8 +42,8 @@ const NameOfLab = () => {
                 <br /><br /><br /><br /><br />
                   <div className='flex flex-col gap-2 w-1/2 justify-center m-auto h-72 border-2 bg-slate-300 border-2 border-blue-700 items-center'>
                     <div className='text-xl font-semibold'>Name Of Lab:</div>
-                    <div className='w-full'><input type="text" placeholder='Enter Name Of Lab...' className='w-3/4 flex justify-center mx-auto h-8 p-2 border-2 bg-slate-100 border-blue-600 font-semibold outline-0' /></div>
-                    <div><button className='px-4 py-1 bg-indigo-700 hover:bg-indigo-900 text-white rounded-md mt-1'>Submit</button></div>
+                    <div className='w-full'><input type="text" placeholder='Enter Name Of Lab...' className='w-3/4 flex justify-center mx-auto h-8 p-2 border-2 bg-slate-100 border-blue-600 font-semibold outline-0' onChange={(e)=>setLabName(e.target.value)} /></div>
+                    <div><button className='px-4 py-1 bg-indigo-700 hover:bg-indigo-900 text-white rounded-md mt-1' onClick={handleSubmit}>Submit</button></div>
                   </div>
               </div>
           </div>
