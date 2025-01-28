69fbeedb0e53dece149d027efb31fdb907625969
Siddharth Srivastava <143588718+Siddharth4501@users.noreply.github.com>
2025-01-28 20:48:08 +0530

    added administration reducer

--- Client/src/Redux/store.js
+++ Client/src/Redux/store.js
@@ -3,13 +3,16 @@ import authSliceReducer from './Slices/AuthSlice.js'
 import groupSliceReducer from './Slices/GroupSilce.js'
 import sampleSliceReducer from './Slices/SampleSlice.js'
 import substanceSliceReducer from "./Slices/SubstanceSlice.js";
+import extraSliceReducer from "./Slices/ExtraSlice.js";
+
 
 const store=configureStore({
     reducer:{
         auth:authSliceReducer,
         group:groupSliceReducer,
         sample:sampleSliceReducer,
-        substance:substanceSliceReducer
+        substance:substanceSliceReducer,
+        administration:extraSliceReducer,
     },
     devtools:true
 });
