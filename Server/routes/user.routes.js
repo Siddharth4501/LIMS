fafe72e7d797cc5c4bf30c9cab648455382883f4
Siddharth Srivastava <143588718+Siddharth4501@users.noreply.github.com>
2025-02-01 20:53:16 +0530

    added reset password route

--- Server/routes/user.routes.js
+++ Server/routes/user.routes.js
@@ -1,5 +1,5 @@
 import {Router} from "express";
-import { Login,Register,Logout, UserData, changePassword,DeleteUserData,DeleteUserRole,UpdateUser } from "../controllers/user.controller.js";
+import { Login,Register,Logout, UserData, changePassword,DeleteUserData,DeleteUserRole,UpdateUser,resetPassword } from "../controllers/user.controller.js";
 import { isLoggedIn } from "../middlewares/auth.middleware.js";
 
 const router=Router()
@@ -8,6 +8,7 @@ router.post('/register',Register)
 router.post('/login',Login)
 router.put('/update',UpdateUser)
 router.post('/Change-Password',isLoggedIn,changePassword)
+router.post('/Reset-Password',resetPassword)
 router.post('/delete',DeleteUserData)
 router.post('/role/delete',DeleteUserRole)
 router.get('/data',UserData)
