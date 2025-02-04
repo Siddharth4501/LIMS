0f4d2300d5993969509d301349066d4222b1d882
Siddharth Srivastava <143588718+Siddharth4501@users.noreply.github.com>
2025-02-04 23:45:59 +0530

    added route to upload Analysis data file

--- Server/routes/Sample.routes.js
+++ Server/routes/Sample.routes.js
@@ -1,9 +1,11 @@
 import {Router} from "express";
-import { SampleRegister,SampleData,SampleEdit,TMDataSave, TMANData,TMANDataUpdate, DeleteSampleData } from "../controllers/Sample.controller.js";
+import { SampleRegister,SampleData,SampleEdit,TMDataSave, TMANData,TMANDataUpdate, DeleteSampleData,uploadFile } from "../controllers/Sample.controller.js";
+import upload from "../middlewares/multer.middleware.js";
 const router=Router()
 
 router.post('/register',SampleRegister)
 router.post('/TM/data/save',TMDataSave)
+router.post('/File/Upload',upload.single("sampleDetailsFile"),uploadFile)
 router.get('/TMAN/data',TMANData)
 router.get('/data',SampleData)
 router.post('/delete',DeleteSampleData)
