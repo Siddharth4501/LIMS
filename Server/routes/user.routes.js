b8a767c5b3eff81b2ca255c2c26f3d1148d920c1
Siddharth Srivastava <143588718+Siddharth4501@users.noreply.github.com>
2025-02-06 20:44:45 +0530

    added route for admin registration

--- Server/routes/user.routes.js
+++ Server/routes/user.routes.js
@@ -1,10 +1,11 @@
 import {Router} from "express";
-import { Login,Register,Logout, UserData, changePassword,DeleteUserData,DeleteUserRole,UpdateUser,resetPassword } from "../controllers/user.controller.js";
+import { Login,Register,Logout, UserData, changePassword,DeleteUserData,DeleteUserRole,UpdateUser,resetPassword,AdminRegister } from "../controllers/user.controller.js";
 import { isLoggedIn } from "../middlewares/auth.middleware.js";
 
 const router=Router()
 
 router.post('/register',Register)
+router.post('/Admin/register',AdminRegister)
 router.post('/login',Login)
 router.put('/update',UpdateUser)
 router.post('/Change-Password',isLoggedIn,changePassword)
