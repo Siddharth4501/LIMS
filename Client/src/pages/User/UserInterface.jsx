72feef5f306f147dba0dd6b2ed03fb08e4ee6667
Siddharth Srivastava <143588718+Siddharth4501@users.noreply.github.com>
2025-02-06 20:47:28 +0530

    added use rcomon nav

--- Client/src/pages/User/UserInterface.jsx
+++ Client/src/pages/User/UserInterface.jsx
@@ -1,70 +1,12 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { logout } from '../../Redux/Slices/AuthSlice';
-import {useDispatch} from 'react-redux';
-import toast from 'react-hot-toast';
-const UserInterface = () => {
-    const userData=JSON.parse(localStorage.getItem('userData'));
-    const dispatch=useDispatch()
-    const navigate = useNavigate();
-    const [selectedRole, setSelectedRole] = useState('');
+import React from 'react';
 
-    const handleChange = (event) => {
-        const role = event.target.value;
-        setSelectedRole(role); // Update the selected role
-        navigate(`/${role}/Home`); // Navigate to the dynamic route
-    };
-    const handleLogout=async(e)=>{
-        const value=e.target.value;
-        if(value==='logout'){
-            const res=await dispatch(logout())
-            if(res?.payload?.success){
-                navigate('/Login')
-                toast.success("Successfully Logged Out");
-                
-            }
-            else{
-                toast.error("Something Went Wrong");
-            }
-        }
-    else if(value=='change-password')
-        navigate('/User/Change-Password')
-    }
+import UserCommonNav from '../../components/UserCommonNav.jsx';
+const UserInterface = () => {
+    
     return (
         <div className="w-screen bg-no-repeat h-screen">
-
-            <div className="flex sm:flex-row flex-col items-center justify-between p-4 shadow-md bg-slate-500 border border-2 border-slate-800">
-
-                <div className="flex items-center">
-                    <img src=" /src/assets/images/DIBT.jpg" alt="Logo"
-                        className="h-20 w-22 object-contain mr-8 ml-4 rounded-full"
-                    />
-                    <span className="text-lg font-bold">Name of Lab : DFRL</span>
-                </div>
-
-                <div className='flex sm:flex-row flex-col '>
-                    <h2 className='pt-2 text-gray-800 text-lg font-bold sm:mx-0 mx-auto'>Change Role</h2>
-                    <div className='px-2 mr-6 sm:ml-0 ml-5 mb-1'>
-                        <select name="" id="" className="p-2 rounded-lg border-2 outline-0 border-blue-600" value={selectedRole} onChange={handleChange}>
-                            <option value="" className='cursor-not-allowed' disabled>Select a role</option>
-                            {
-                                userData.roles.map((item)=>{
-                                    return <option value={item.designation} key={item._id}>{item.designation}</option>
-                                })
-                            }
-                        </select>
-                    </div>
-                    <div className='sm:mx-6 mx-auto'>
-                        <select name="" id="" className="p-2 rounded-lg border-2 outline-0 border-blue-600" onChange={handleLogout}>
-                            <option value={userData.fullName}>{userData.fullName.toUpperCase()}</option>
-                            <option value="change-password">Change Password</option>
-                            <option value="logout" className='text-red-500'>Log Out!</option>
-                        </select>
-                    </div>
-                </div>
-            </div>
-            <div className=" min-h-[84vh] w-screen bg-[url('/src/assets/images/LIMS-BACK4.webp')] bg-cover bg-top">
-                
+            <UserCommonNav/>
+            <div className=" min-h-[84vh] w-screen bg-[url('/src/assets/images/LIMS-BACK4.webp')] bg-cover bg-top">   
             </div>
         </div>
     );
--- Client/src/pages/User/UserSampleRegister.jsx
+++ Client/src/pages/User/UserSampleRegister.jsx
@@ -1,71 +1,28 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import toast from 'react-hot-toast';
-import { logout } from '../../Redux/Slices/AuthSlice';
+import UserCommonNav from '../../components/UserCommonNav';
 
 const UserSampleRegister = () => {
-  const dispatch = useDispatch();
   const navigate = useNavigate();
-  const userData = JSON.parse(localStorage.getItem('userData'));
   const handleSampleHistoryPage = () => {
     navigate('/SampleRegistrationUser/SampleHistory')
   }
   const handleSampleRegister = () => {
     navigate('/SampleRegister')
   }
-  const handleRedirection = async (e) => {
-    const value = e.target.value;
-    if (value === 'User Home') {
-      navigate('/')
-    }
-    else if (value === 'logout') {
-      const res = await dispatch(logout())
-      if (res?.payload?.success) {
-        navigate('/Login')
-        toast.success("Successfully Logged Out");
-
-      }
-      else {
-        toast.error("Something Went Wrong");
-      }
-    }
-    else if (value == 'change-password') {
-      navigate('/User/Change-Password')
-    }
-  }
   return (
     <>
-      <div className="w-screen h-screen bg-[url('/src/assets/images/DRDODIBT-BACK.png')] bg-cover bg-center">
-        <div className="flex items-center justify-between p-4 shadow-md bg-slate-500 border border-2 border-slate-800">
-          <div className="flex items-center">
-            <img src="/src/assets/images/DRDO-Logo1.jpg" alt="Logo"
-              className="h-20 w-22 object-contain mr-8 ml-4 rounded-full"
-              />
-            <span className="text-lg font-bold">Name of Lab: DFRL</span>
-          </div>
-          <div>
-            <h1 className='text-center font-medium text-3xl p-4'>Sample Registration</h1>
-          </div>
-          <div className='mx-6'>
-            <select name="" id="" className="p-2 rounded-lg border-2 outline-0 border-blue-600" onChange={handleRedirection}>
-              <option value="name">{userData.fullName.toUpperCase()}</option>
-              <option value="User Home">Home</option>
-              <option value="change-password">Change Password</option>
-              <option value="logout" className='text-red-500'>Log Out!</option>
-            </select>
-          </div>
-
-        </div>
+      <div className="w-screen h-screen bg-[url('/src/assets/images/DRDODIBT-BACK.png')] bg-cover bg-center bg-fixed">
+        <UserCommonNav assignedRole='Sample Registration'/>
         <br /><br /><br /><br /><br />
         <div className='grid gap-12 mt-16 px-4 py-2 m-auto w-3/4'>
           <center>
-            <button className='bg-indigo-700 shadow-lg text-white hover:bg-indigo-900 w-80 px-4 py-2 rounded-md text-2xl font-normal transition-transform duration-300 ease-in-out hover:scale-105' onClick={handleSampleRegister}>
+            <button className='bg-indigo-700 shadow-lg text-white hover:bg-indigo-800 w-80 px-4 py-2 rounded-md text-2xl font-normal transition-transform duration-300 ease-in-out hover:scale-105' onClick={handleSampleRegister}>
               Sample Register
             </button>
           </center>
           <center>
-            <button className='bg-indigo-700 shadow-lg text-white hover:bg-indigo-900 w-80 px-4 py-2 rounded-md text-2xl font-normal transition-transform duration-300 ease-in-out hover:scale-105' onClick={handleSampleHistoryPage}>
+            <button className='bg-indigo-700 shadow-lg text-white hover:bg-indigo-800 w-80 px-4 py-2 rounded-md text-2xl font-normal transition-transform duration-300 ease-in-out hover:scale-105' onClick={handleSampleHistoryPage}>
               Sample History Page
             </button>
           </center>
