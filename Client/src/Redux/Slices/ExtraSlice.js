8a85466e93e3f78780dca78e67fe5aaa4a1ac90d
Siddharth Srivastava <143588718+Siddharth4501@users.noreply.github.com>
2025-02-06 20:52:02 +0530

    added save Error function

--- Client/src/Redux/Slices/ExtraSlice.js
+++ Client/src/Redux/Slices/ExtraSlice.js
@@ -4,7 +4,8 @@ import axios from "axios";
 
 const initialState={
     LabNameData:[],
-    logoData:[]
+    logoData:[],
+    errorData:[]
 }
 
 // there is no need to make a reducer for the signup action because we dont neet to store anything in store ,we simply pefomt async operation so thunk is enough for it
@@ -79,6 +80,56 @@ export const getNameOfLab = createAsyncThunk("NameOfLab/data", async () => {
     }
   });
 
+  export const saveError = createAsyncThunk("Error/save", async (data) => {
+    try {
+      let res = axios.post("http://localhost:5001/api/v1/Error/add",data,{
+        withCredentials: true, // Include cookies
+      })
+      toast.promise(res, {
+        loading: "Wait! Adding Error",
+        success: (data) => {
+          return data?.data?.message;
+        },
+        error: "Failed Add Error",
+      });
+  
+      // getting response resolved here
+      res = await res;
+      return res.data;
+    } catch (error) {
+      toast.error(error?.response?.data?.message);
+    }
+  });
+
+  export const getError = createAsyncThunk("Error/get", async () => {
+    try {
+      let res = axios.get("http://localhost:5001/api/v1/Error/data",{
+        withCredentials: true, // Include cookies
+      })
+      
+      // getting response resolved here
+      res = await res;
+      return res.data;
+    } catch (error) {
+      toast.error(error?.response?.data?.message);
+    }
+  });
+  export const deleteError = createAsyncThunk("Error/delete", async ({ errorID }, { dispatch }) => {
+    try {
+      const id=errorID;
+      let res = axios.delete(`http://localhost:5001/api/v1/Error/delete/${id}`,{
+        withCredentials: true, // Include cookies
+      })
+      
+      // getting response resolved here
+      res = await res;
+      dispatch(getError());
+      return res.data;
+    } catch (error) {
+      toast.error(error?.response?.data?.message);
+    }
+  });
+
 const extraSlice=createSlice({
       name:'administration',
       initialState,
@@ -100,9 +151,15 @@ const extraSlice=createSlice({
                 state.logoData = action.payload.logo;
             }
           })
+          .addCase(getError.fulfilled, (state, action) => {
+            //action grabbed response returned from thunk
+            //action.payload is json response from backend object coantaining success,message,data here (group)
+            if (action.payload) {
+                state.errorData = action.payload.error;
+            }
+          })
         }
         })
-  
   //named export
   //export const {}=extraSlice.actions;
   
