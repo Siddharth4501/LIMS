be00f9c10098a543356ae054eea3b6008f4d3b7a
Siddharth Srivastava <143588718+Siddharth4501@users.noreply.github.com>
2025-02-06 20:47:50 +0530

    added route to delete Error

--- Server/routes/Error.routes.js
+++ Server/routes/Error.routes.js
@@ -1,8 +1,9 @@
 import {Router} from "express";
-import { ErrorAdd, FetchError } from "../controllers/Error.controller.js";
+import { ErrorAdd, FetchError,DeleteError } from "../controllers/Error.controller.js";
 
 const router=Router()
 
 router.post('/add',ErrorAdd)
 router.get('/data',FetchError)
+router.delete('/delete/:id',DeleteError)
 export default router;
