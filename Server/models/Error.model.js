09f785760efc81ae2e742e46c327a13ca56d01d1
Siddharth Srivastava <143588718+Siddharth4501@users.noreply.github.com>
2025-02-06 20:48:47 +0530

    changed error model

--- Server/models/Error.model.js
+++ Server/models/Error.model.js
@@ -1,16 +1,20 @@
 // superset
-import {model,Schema} from 'mongoose'
+import {model,Schema,mongoose} from 'mongoose'
 
 const errorSchema=new Schema(
     {
-        Error_Message:{
-            type:String,
-            required:[true,'Error_Message is required'],
-        },
+        Group:{
+            type: mongoose.Schema.Types.ObjectId, ref: "Group"
+        }, 
+        Group_Name:String,
         Type_Of_Testing:{
             type:String,
             required:[true,'Type of Testing name is required'],
         },    
+        Error_Message:{
+            type:String,
+            required:[true,'Error_Message is required'],
+        },
     },
     {
         timestamps:true
